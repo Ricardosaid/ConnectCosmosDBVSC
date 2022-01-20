c8c5fda264449e70a3a1c184809255379bbb0764
RicardoSMS <rsmartinez@bside.com.mx>
2022-01-19 22:56:57 -0600

    Added console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConnectCosmosDBVSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
